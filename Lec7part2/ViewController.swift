0c337eed859dd7a98f07222b3cedc1e4cc911bb1
roeyHonig <roeyhonig94@gmail.com>
2018-05-14 13:28:09 +0300

    Initial Commit

---
+++ Lec7part2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lec7part2
+//
+//  Created by hackeru on 29 Iyar 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
